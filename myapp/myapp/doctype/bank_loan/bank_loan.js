224fa1976e09133e10a9f52c6a0e7295a981c855
OHany1999 <omar1999410@gmail.com>
2024-12-26 11:22:11 +0200

    omar change

--- myapp/myapp/doctype/bank_loan/bank_loan.js
+++ myapp/myapp/doctype/bank_loan/bank_loan.js
@@ -1,27 +1,27 @@
 // Copyright (c) 2024, omar and contributors
 // For license information, please see license.txt
+//aaaaaaaaaaaaaaaaaaaaaa
+frappe.ui.form.on('Bank Loan', {
+	// refresh: function(frm) {
 
-frappe.ui.form.on("Bank Loan", {
-	// refresh(frm) {
-
-	// },
-
-    repayment_months : function(frm){calculate_monthely_repayment(frm)},
-    loan_amount : function(frm){calculate_monthely_repayment(frm)},
-    interest : function(frm){calculate_monthely_repayment(frm)},
-    
+	// }
+	repayment_months : function(frm){calculate(frm)},
+	loan_amount : function(frm){calculate(frm)},
+	interest : function(frm){calculate(frm)},
+	
 });
 
+function calculate(frm){
 
-function calculate_monthely_repayment(frm){
-    if(frm.doc.repayment_months && frm.doc.loan_amount){
-        var monthly = (frm.doc.loan_amount / frm.doc.repayment_months);
-        var monthly = monthly + monthly * frm.doc.interest / 100;
-        frm.doc.monthely_repayment = monthly;
-        // refresh_field("monthly_repayment");
-        frm.set_value("monthely_repayment", monthly);
-    }else{
-        frm.doc.monthely_repayment = 0;
-        // refresh_field("monthly_repayment");
-    }
+if (frm.doc.loan_amount && frm.doc.repayment_months){
+			var monthly=(frm.doc.loan_amount/ frm.doc.repayment_months);
+			var monthly = monthly+monthly*frm.doc.interest/100;
+            print(monthly);
+			frm.doc.monthly_repayment=monthly;
+			refresh_field("monthly_repayment");
+		}else{
+		frm.doc.monthly_repayment=0;
+			refresh_field("monthly_repayment");
+	}
+
 }
--- myapp/myapp/doctype/bank_loan/bank_loan.py
+++ myapp/myapp/doctype/bank_loan/bank_loan.py
@@ -1,9 +1,13 @@
 # Copyright (c) 2024, omar and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class BankLoan(Document):
-	pass
+	
+	def on_update(self):
+		self.schedule = []
+		
+			
